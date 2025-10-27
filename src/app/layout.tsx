a1428480d1e9dddec49dce659cb79c31fdf7054e
narcisse66 <dossanarcisse242@gmail.com>
2025-10-27 22:53:19 +0100

    update opengraph3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,25 +14,29 @@ const oswald = Oswald({
 });
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://g-league-l465.vercel.app'), 
   title: 'G-League - Basketball League Website',
   description:
     'A website that showcases the GravityLeague (G-League) informations, including teams, matches, player stats, and the latest news.',
-
-
-
-
   openGraph: {
     title: 'G-League - Basketball League Website',
     description: 'A website that showcases the GravityLeague (G-League) informations, including teams, matches, player stats, and the latest news.',
-    url: 'https://g-league-l465.vercel.app', 
+    url: '/', 
     siteName: 'G-League',
     images: [
-      { url: 'https://g-league-l465.vercel.app/images/logos/logo7white.png', width: 1200, height: 630 },
+      { url: '/images/logos/logo7white.png', width: 1200, height: 630 },
     ],
     type: 'website',
     locale: 'en_US',
   },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'G-League - Basketball League Website',
+    description: 'A website that showcases the GravityLeague (G-League) informations...',
+    images: ['/images/logos/logo7white.png'], 
+  },
 };
+
 
 export default function RootLayout({
   children,
